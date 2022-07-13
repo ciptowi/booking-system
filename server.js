a8fc40ca69ffeb803f8280a29a7382363bac5d74
cipto <ciptowidarto@gmail.com>
2022-07-13 12:54:10 +0700

    config deploy vercel

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 // routes
 app.use(router);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
